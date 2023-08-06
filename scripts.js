78bd96b55827d5e2ba86a5bd28ace6f603321ee5
Tiffany Hill <hill.tiffany365@gmail.com>
2023-08-06 18:24:28 -0400

    Number 2 of studio done

--- scripts.js
+++ scripts.js
@@ -1,2 +1,23 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    //BUTTONS
+    const takeOffButton = document.getElementById("takeoff");
+
+    //AREAS
+    const flightStatus = document.getElementById("flightStatus");
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+
+    takeOffButton.addEventListener("click", function() {
+
+        let shouldTakeOff = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        //console.log(shouldTakeOff);
+
+        if (shouldTakeOff) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            shuttleBackground.style.backgroundColor = "blue";
+            spaceShuttleHeight.innerHTML = "10,000";
+        }
+    });
+});
